9af84a558d2544b6936e0ddefb1ee29496454c3b
Tushar Chandra <me@tusharc.dev>
2020-05-24 02:04:47 -0400

    Fix so that npm run build doesn't run postcss

--- postcss.config.js
+++ postcss.config.js
@@ -2,12 +2,5 @@ module.exports = {
     plugins: [
         require('tailwindcss'),
         require('autoprefixer'),
-        require('@fullhuman/postcss-purgecss')({
-            content: [
-                "index.html",
-                "js/script.js",
-                "tailwind.config.js",
-            ]
-        })
     ]
 }
